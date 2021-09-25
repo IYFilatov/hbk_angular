5b1f6cf2bd65cb92679d6f876d441dbaa4494240
Abrader <akaAbrader@gmail.com>
2021-09-25 16:11:15 +0300

    update aot captions

--- src/app/modules/main-frame/components/aot/aot.component.ts
+++ src/app/modules/main-frame/components/aot/aot.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { AotService } from '../../../../core/services/aot.service';
 import { aotElement } from '../../../../shared/models/aot-element';
+import conf from '../../../../core/configs';
 
 @Component({
   selector: 'app-aot',
@@ -27,7 +28,8 @@ export class AotComponent implements OnInit {
   getDictList(baseId: string){
     this.aotService.getDictList(baseId).subscribe(
       dictList => {
-        this.dictList = dictList        
+        this.dictList = dictList;
+        this.setDictCaptions();
       },
       err => {
         //this.dictList = JSON.parse(err.error).message;
@@ -38,8 +40,23 @@ export class AotComponent implements OnInit {
 
   getDocList(baseId: string){
     this.aotService.getDocList(baseId).subscribe(
-      docList => { this.docList = docList }
+      docList => { 
+        this.docList = docList; 
+        this.setDocCaptions();
+      }
     );
+  }
+
+  setDictCaptions(){
+    this.dictList.forEach(v => {
+      v.caption = conf.dictionaries[v.name].caption;
+    });
+  }
+
+  setDocCaptions(){
+    this.docList.forEach(v => {
+      v.caption = conf.documents[v.name].caption;
+    });
   }
 
   openDictJournal(dictName: string): void {
--- src/app/shared/models/aot-element.ts
+++ src/app/shared/models/aot-element.ts
@@ -1,4 +1,5 @@
 export interface aotElement {
   type: string;
   name: string;
+  caption: string;
 }
